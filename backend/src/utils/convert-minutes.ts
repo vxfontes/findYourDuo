2ca79dbe5a2b7054e3b3a369a169f7aa23c1d8e4
vxfontes <nessa1vane@icloud.com>
2022-09-15 02:53:20 -0300

    adicionando padStart para manipulação do zero

--- backend/src/utils/convert-minutes.ts
+++ backend/src/utils/convert-minutes.ts
@@ -2,5 +2,5 @@ export function convertMinutesToHoursString(minutesAmount: number) {
     const hours = Math.floor(minutesAmount / 60);
     const minutes = minutesAmount % 60;
 
-    return `${hours}:${minutes}`;
+    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 }
